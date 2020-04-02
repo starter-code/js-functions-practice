9cf23b6e9e7ff3f2ddcbb62836d36adc93c6b545
David Ko <david.holy.ko@gmail.com>
2020-04-02 18:17:50 -0400

    add solutions
    update functions

--- src/js-functions-1.js
+++ src/js-functions-1.js
@@ -1,26 +1,42 @@
-const shouldRunMain = process.argv[2];
+const shouldRunMain = process.argv[3];
 
 // expects no argument
 // example usage: hello()
+// expected output: console logs "Hello World!"
+// expected return: undefined
 const hello = () => {
   // fill in this function
 };
 
 // expects a string argument
 // example usage: helloString("Bye!")
+// expected output: console logs "Bye!"
+// expected return: undefined
 const helloString = inputString => {
   // fill in this function
 };
 
 // expects an array of strings argument
 // example usage: helloString(["Bye!", "Hi!"])
+// expected output: console logs "Bye!" and then console logs "Hi!"
+// expected return: undefined
 const helloArray = inputArray => {
   // fill in this function
 };
 
 // expects an object with key "message" argument
 // example usage: helloString({message: "Hello"})
+// expected output: console logs "Hello World!"
+// expected return: undefined
 const helloObject = inputObject => {
+  // fill in this function
+};
+
+// expects 3 arguments
+// example usage: helloString("Hello", "World", "!")
+// expected output: console logs "Hello World!"
+// expected return: undefined
+const helloArguments = (argumentOne, argumentTwo, argumentThree) => {
   // fill in this function
 };
 
@@ -29,10 +45,12 @@ const helloObject = inputObject => {
     return;
   }
 
+  console.log('Starting Main');
   hello();
-  helloString(); // fill in argument
-  helloArray(); // fill in argument
-  helloObject(); // fill in argument
+  helloString(''); // replace arguments
+  helloArray([]); // replace arguments
+  helloObject({}); // replace arguments
+  helloArguments('', '', ''); // replace argumentss
 })();
 
 module.exports = {
@@ -40,4 +58,5 @@ module.exports = {
   helloString,
   helloArray,
   helloObject,
+  helloArguments,
 };
--- tests/js-functions-1.test.js
+++ tests/js-functions-1.test.js
@@ -3,18 +3,36 @@ const expect = require('chai').expect;
 const jsFunctions = require('../src/js-functions-1');
 require('mocha-sinon');
 
-const expectedOutput = {
+const expectedOutputOne = {
   hello: 'Hello World!',
   helloString: 'Hello!',
   helloArray: ['Hi!', 'Hello there!'],
   helloObject: 'Hi World!',
+  helloArguments: 'Hi World !',
 };
 
-const params = {
+const expectedOutputTwo = {
+  hello: 'Hello World!',
+  helloString: 'Hello2!',
+  helloArray: ['Hi2!', 'Hello there2!', 'Hello2!'],
+  helloObject: 'Hi World2!',
+  helloArguments: 'Hello there person!',
+};
+
+const paramsOne = {
   hello: undefined,
   helloString: 'Hello!',
   helloArray: ['Hi!', 'Hello there!'],
   helloObject: { message: 'Hi World!' },
+  helloArguments: ['Hi', 'World', '!'],
+};
+
+const paramsTwo = {
+  hello: 'Hello World!',
+  helloString: 'Hello2!',
+  helloArray: ['Hi2!', 'Hello there2!', 'Hello2!'],
+  helloObject: { message: 'Hi World2!' },
+  helloArguments: ['Hello', 'there', 'person!'],
 };
 
 const originalLog = console.log;
@@ -27,28 +45,60 @@ describe('JS Functions Practice: Hello World!', function() {
     console.log = originalLog;
   });
 
-  it('hello should log "Hello World!', () => {
+  it('hello should log "Hello World!"', () => {
     jsFunctions.hello();
     expect(console.log.calledOnce).to.be.true;
-    expect(console.log.calledWith(expectedOutput.hello)).to.be.true;
+    expect(console.log.calledWith(expectedOutputOne.hello)).to.be.true;
   });
 
-  it('helloString should log "Hello!', () => {
-    jsFunctions.helloString(params.helloString);
+  it('helloString should log "Hello!"', () => {
+    jsFunctions.helloString(paramsOne.helloString);
     expect(console.log.calledOnce).to.be.true;
-    expect(console.log.calledWith(expectedOutput.helloString)).to.be.true;
+    expect(console.log.calledWith(expectedOutputOne.helloString)).to.be.true;
   });
 
-  it('helloArray should log "Hi! and "Hello there!"', () => {
-    jsFunctions.helloArray(params.helloArray);
+  it('helloString should log "Hello2!"', () => {
+    jsFunctions.helloString(paramsTwo.helloString);
+    expect(console.log.calledOnce).to.be.true;
+    expect(console.log.calledWith(expectedOutputTwo.helloString)).to.be.true;
+  });
+
+  it('helloArray should log "Hi!" and "Hello there!"', () => {
+    jsFunctions.helloArray(paramsOne.helloArray);
     expect(console.log.calledTwice).to.be.true;
-    expect(console.log.calledWith(expectedOutput.helloArray[0])).to.be.true;
-    expect(console.log.calledWith(expectedOutput.helloArray[1])).to.be.true;
+    expect(console.log.calledWith(expectedOutputOne.helloArray[0])).to.be.true;
+    expect(console.log.calledWith(expectedOutputOne.helloArray[1])).to.be.true;
   });
 
-  it('hello should log "Hi World!', () => {
-    jsFunctions.helloObject(params.helloObject);
+  it('helloArray should log "Hi2!" and "Hello there2!" and "Hello2!"', () => {
+    jsFunctions.helloArray(paramsTwo.helloArray);
+    expect(console.log.calledThrice).to.be.true;
+    expect(console.log.calledWith(expectedOutputTwo.helloArray[0])).to.be.true;
+    expect(console.log.calledWith(expectedOutputTwo.helloArray[1])).to.be.true;
+    expect(console.log.calledWith(expectedOutputTwo.helloArray[2])).to.be.true;
+  });
+
+  it('helloObject should log "Hi World!"', () => {
+    jsFunctions.helloObject(paramsOne.helloObject);
     expect(console.log.calledOnce).to.be.true;
-    expect(console.log.calledWith(expectedOutput.helloObject)).to.be.true;
+    expect(console.log.calledWith(expectedOutputOne.helloObject)).to.be.true;
+  });
+
+  it('helloObject should log "Hi World2!"', () => {
+    jsFunctions.helloObject(paramsTwo.helloObject);
+    expect(console.log.calledOnce).to.be.true;
+    expect(console.log.calledWith(expectedOutputTwo.helloObject)).to.be.true;
+  });
+
+  it('helloArguments should log "Hi World !"', () => {
+    jsFunctions.helloArguments(...paramsOne.helloArguments);
+    expect(console.log.calledOnce).to.be.true;
+    expect(console.log.calledWith(...paramsOne.helloArguments)).to.be.true;
+  });
+
+  it('helloArguments should log "Hello there person!"', () => {
+    jsFunctions.helloArguments(...paramsTwo.helloArguments);
+    expect(console.log.calledOnce).to.be.true;
+    expect(console.log.calledWith(...paramsTwo.helloArguments)).to.be.true;
   });
 });
